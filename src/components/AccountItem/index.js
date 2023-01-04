72ded4c4f697ed78bc5a50e0f688f6a6849aa961
Mai Mai <ntmainbm6328@gmail.com>
2023-01-04 22:28:26 +0700

    UI Header

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+  return (
+    <div className={cx("wrapper")}>
+      <img
+        className={cx("avatar")}
+        src="https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&w=300"
+        alt="Hoaaa"
+      />
+      <div className={cx("info")}>
+        <h4 className={cx("name")}>
+          <span> Nguyen Van A</span>
+          <FontAwesomeIcon className={cx("checkIcon")} icon={faCheckCircle} />
+        </h4>
+
+        <span className={cx("username")}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
---
+++ src/components/Button/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./Button.module.scss";
+import { Link } from "react-router-dom";
+
+const cx = classNames.bind(styles);
+
+// to tương ứng với link nội bộ. href tương ứng với link bên ngoài
+
+function Button({
+  to,
+  href,
+  primary = false,
+  outline = false,
+  text = false,
+  rounded = false,
+  disabled = false,
+  small = false,
+  medium = false,
+  large = false,
+  children,
+  className,
+  leftIcon,
+  rightIcon,
+  onClick,
+  ...passProps
+}) {
+  let Component = "button";
+  const props = {
+    onClick,
+    ...passProps,
+  };
+
+  // remove event listener when btn is disabled
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith("on") && typeof props[key] === "function") {
+        delete props[key];
+      }
+    });
+  }
+
+  if (to) {
+    props.to = to;
+    Component = Link;
+  } else if (href) {
+    props.href = href;
+    Component = "a";
+  }
+
+  const classes = cx("wrapper", {
+    [className]: className,
+    primary: primary,
+    outline: outline,
+    text,
+    rounded,
+    disabled,
+    small,
+    medium,
+    large,
+  });
+  return (
+    <div>
+      <Component className={classes} {...props}>
+        {leftIcon && <span className={cx("icon")}>{leftIcon}</span>}
+        <span className={cx("title")}>{children}</span>
+        {rightIcon && <span className={cx("icon")}>{rightIcon}</span>}
+      </Component>
+    </div>
+  );
+}
+
+export default Button;
